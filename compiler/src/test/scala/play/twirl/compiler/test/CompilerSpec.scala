374db4d767f328000713edcef6fae81236b697ea
Peter Vlugter <pvlugter@gmail.com>
2014-05-20 18:52:19 +1200

    Use the same default implicit codec for all file encoding

--- compiler/src/test/scala/play/twirl/compiler/test/CompilerSpec.scala
+++ compiler/src/test/scala/play/twirl/compiler/test/CompilerSpec.scala
@@ -91,6 +91,12 @@ abstract class CompilerTests(oldParser: Boolean = false) extends Specification {
       result must be_==("<p>file: example, url: http://example.org</p>")
     }
 
+    "compile successfully (utf8)" in {
+      val helper = newCompilerHelper
+      val text = helper.compile[(() => Html)]("utf8.scala.html", "html.utf8")().toString.trim
+      text must be_==("€, ö, or ü")
+    }
+
     "fail compilation for error.scala.html" in {
       val helper = newCompilerHelper
       helper.compile[(() => Html)]("error.scala.html", "html.error") must throwA[CompilationError].like {
