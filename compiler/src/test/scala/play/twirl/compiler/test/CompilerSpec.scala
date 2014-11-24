c2b0cab5e5d841f5298f8e68c7f9e4a3addb16ea
Peter Vlugter <pvlugter@gmail.com>
2014-11-25 12:16:43 +1300

    Add (failing) test for existential types

--- compiler/src/test/scala/play/twirl/compiler/test/CompilerSpec.scala
+++ compiler/src/test/scala/play/twirl/compiler/test/CompilerSpec.scala
@@ -97,6 +97,12 @@ abstract class CompilerTests(oldParser: Boolean = false) extends Specification {
       text must be_==("€, ö, or ü")
     }
 
+    "compile successfully (existential)" in {
+      val helper = newCompilerHelper
+      val text = helper.compile[(List[_] => Html)]("existential.scala.html", "html.existential")(List(1, 2, 3)).toString.trim
+      text must be_==("123")
+    }
+
     "fail compilation for error.scala.html" in {
       val helper = newCompilerHelper
       helper.compile[(() => Html)]("error.scala.html", "html.error") must throwA[CompilationError].like {
