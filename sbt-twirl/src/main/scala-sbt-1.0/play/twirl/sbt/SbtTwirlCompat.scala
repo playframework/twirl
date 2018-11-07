11a7e30aead6b0b2257b6065c0ad5996c15bd57f
Marcos Pereira <marcos.silva@gmail.com>
2018-11-07 14:28:52 -0500

    Make the fix support sbt 0.13 and 1

---
+++ sbt-twirl/src/main/scala-sbt-1.0/play/twirl/sbt/SbtTwirlCompat.scala
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package play.twirl.sbt
+
+import sbt._
+import sbt.Keys._
+import play.twirl.sbt.Import.TwirlKeys._
+
+object SbtTwirlCompat {
+  def watchSourcesSettings:  Seq[Setting[_]] = Seq(
+    watchSources in Defaults.ConfigGlobal +=
+      WatchSource(
+        (sourceDirectory in compileTemplates).value,
+        (includeFilter in compileTemplates).value,
+        (excludeFilter in compileTemplates).value
+      )
+  )
+}
