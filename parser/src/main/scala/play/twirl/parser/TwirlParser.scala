50978951de039ac486fc425a6d069fc1d2af1d92
Marcos Pereira <marcos.silva@gmail.com>
2019-05-21 18:04:24 -0400

    Scala 2.13.0-RC2 (#217)

--- parser/src/main/scala/play/twirl/parser/TwirlParser.scala
+++ parser/src/main/scala/play/twirl/parser/TwirlParser.scala
@@ -829,7 +829,7 @@ class TwirlParser(val shouldParseInclusiveDot: Boolean) {
         val paramspos = input.offset()
         val types = Option(squareBrackets()) getOrElse PosString("")
         val args = several[String, ArrayBuffer[String]] { () => parentheses() }
-        val params = position(PosString(types + args.mkString), paramspos)
+        val params = position(PosString(types.toString + args.mkString), paramspos)
         if (params != null)
           return (name, params)
       } else input.regress(1) // don't consume @
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.typesafe.play" % "interplay" % sys.props.get("interplay.version").getOrElse("2.0.6"))
+addSbtPlugin("com.typesafe.play" % "interplay" % sys.props.get("interplay.version").getOrElse("2.0.7"))
 
 // For the Cross Build
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.27")
