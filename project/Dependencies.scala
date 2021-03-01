9dd9a0fa5709af4a3879d6ac1cf2015aea7f69ab
Scala Steward <me@scala-steward.org>
2021-03-01 19:16:51 +0100

    scala-compiler, scala-library 2.13.5 (was 2.12.13)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
 
-  val Scala212      = "2.12.13"
+  val Scala212      = "2.13.5"
   val Scala213      = "2.13.4"
   val ScalaVersions = Seq(Scala212, Scala213)
 
