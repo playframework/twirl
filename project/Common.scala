11d9469ddb14031d69c916f2bdf31ca038424947
Matthias Kurz <m.kurz@irregular.at>
2023-03-09 23:27:27 +0100

    Update data for pom.xml

--- project/Common.scala
+++ project/Common.scala
@@ -59,13 +59,13 @@ object Common extends AutoPlugin {
   override def globalSettings =
     Seq(
       organization         := "com.typesafe.play",
-      organizationName     := "Lightbend Inc.",
-      organizationHomepage := Some(url("https://www.lightbend.com/")),
+      organizationName     := "The Play Framework Project",
+      organizationHomepage := Some(url("https://playframework.com/")),
       homepage             := Some(url(s"https://github.com/playframework/${repoName}")),
       licenses             := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
       developers += Developer(
         "playframework",
-        "Play Framework Team",
+        "The Play Framework Contributors",
         "contact@playframework.com",
         url("https://github.com/playframework")
       ),
