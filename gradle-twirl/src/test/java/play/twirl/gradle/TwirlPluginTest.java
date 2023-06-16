d528a9446ac19d7112a00da0418cebc446122a55
Sergey Morgunov <hostage@me.com>
2023-06-16 14:58:58 +0300

    Rename plugin id to `com.playframework.twirl` Rename implementation class to `TwirlPlugin`

--- gradle-twirl/src/test/java/play/twirl/gradle/TwirlPluginTest.java
+++ gradle-twirl/src/test/java/play/twirl/gradle/TwirlPluginTest.java
@@ -10,13 +10,13 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
 
 /**
- * A simple unit test for the 'play.twirl.gradle.greeting' plugin.
+ * A simple unit test for the 'com.playframework.twirl' plugin.
  */
-class GradleTwirlPluginTest {
+class TwirlPluginTest {
     @Test void pluginRegistersATask() {
         // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
-        project.getPlugins().apply("play.twirl.gradle.greeting");
+        project.getPlugins().apply("com.playframework.twirl");
 
         // Verify the result
         assertNotNull(project.getTasks().findByName("greeting"));
