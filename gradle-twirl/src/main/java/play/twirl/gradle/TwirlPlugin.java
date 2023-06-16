63bad5ba4fff6f3991536bcaea7d0e53c804feaf
Sergey Morgunov <hostage@me.com>
2023-06-16 16:59:09 +0300

    Enable Java Format check for Gradle Plugin

--- gradle-twirl/src/main/java/play/twirl/gradle/TwirlPlugin.java
+++ gradle-twirl/src/main/java/play/twirl/gradle/TwirlPlugin.java
@@ -3,17 +3,19 @@
  */
 package play.twirl.gradle;
 
-import org.gradle.api.Project;
 import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 
-/**
- * A simple 'hello world' plugin.
- */
+/** A simple 'hello world' plugin. */
 public class TwirlPlugin implements Plugin<Project> {
-    public void apply(Project project) {
-        // Register a task
-        project.getTasks().register("greeting", task -> {
-            task.doLast(s -> System.out.println("Hello from plugin 'com.playframework.twirl'"));
-        });
-    }
+  public void apply(Project project) {
+    // Register a task
+    project
+        .getTasks()
+        .register(
+            "greeting",
+            task -> {
+              task.doLast(s -> System.out.println("Hello from plugin 'com.playframework.twirl'"));
+            });
+  }
 }
