2649bf8d5bd56df9143b0a8cd045c1f469282467
Sergey Morgunov <hostage@me.com>
2023-06-16 14:58:58 +0300

    Rename plugin id to `com.playframework.twirl` Rename implementation class to `TwirlPlugin`

--- gradle-twirl/src/main/java/play/twirl/gradle/TwirlPlugin.java
+++ gradle-twirl/src/main/java/play/twirl/gradle/TwirlPlugin.java
@@ -9,11 +9,11 @@ import org.gradle.api.Plugin;
 /**
  * A simple 'hello world' plugin.
  */
-public class GradleTwirlPlugin implements Plugin<Project> {
+public class TwirlPlugin implements Plugin<Project> {
     public void apply(Project project) {
         // Register a task
         project.getTasks().register("greeting", task -> {
-            task.doLast(s -> System.out.println("Hello from plugin 'play.twirl.gradle.greeting'"));
+            task.doLast(s -> System.out.println("Hello from plugin 'com.playframework.twirl'"));
         });
     }
 }
