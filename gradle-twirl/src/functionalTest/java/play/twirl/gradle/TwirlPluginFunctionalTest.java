e43de02fdc2955df7ff58c1a6f91fd47addab8e3
Sergey Morgunov <hostage@me.com>
2023-06-16 16:59:09 +0300

    Enable Java Format check for Gradle Plugin

--- gradle-twirl/src/functionalTest/java/play/twirl/gradle/TwirlPluginFunctionalTest.java
+++ gradle-twirl/src/functionalTest/java/play/twirl/gradle/TwirlPluginFunctionalTest.java
@@ -14,43 +14,38 @@ import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-/**
- * A simple functional test for the 'com.playframework.twirl' plugin.
- */
+/** A simple functional test for the 'com.playframework.twirl' plugin. */
 class TwirlPluginFunctionalTest {
-    @TempDir
-    File projectDir;
+  @TempDir File projectDir;
 
-    private File getBuildFile() {
-        return new File(projectDir, "build.gradle");
+  private File getBuildFile() {
+    return new File(projectDir, "build.gradle");
+  }
+
+  private File getSettingsFile() {
+    return new File(projectDir, "settings.gradle");
+  }
+
+  @Test
+  void canRunTask() throws IOException {
+    writeString(getSettingsFile(), "");
+    writeString(getBuildFile(), "plugins {" + "  id('com.playframework.twirl')" + "}");
+
+    // Run the build
+    GradleRunner runner = GradleRunner.create();
+    runner.forwardOutput();
+    runner.withPluginClasspath();
+    runner.withArguments("greeting");
+    runner.withProjectDir(projectDir);
+    BuildResult result = runner.build();
+
+    // Verify the result
+    assertTrue(result.getOutput().contains("Hello from plugin 'com.playframework.twirl'"));
+  }
+
+  private void writeString(File file, String string) throws IOException {
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(string);
     }
-
-    private File getSettingsFile() {
-        return new File(projectDir, "settings.gradle");
-    }
-
-    @Test void canRunTask() throws IOException {
-        writeString(getSettingsFile(), "");
-        writeString(getBuildFile(),
-            "plugins {" +
-            "  id('com.playframework.twirl')" +
-            "}");
-
-        // Run the build
-        GradleRunner runner = GradleRunner.create();
-        runner.forwardOutput();
-        runner.withPluginClasspath();
-        runner.withArguments("greeting");
-        runner.withProjectDir(projectDir);
-        BuildResult result = runner.build();
-
-        // Verify the result
-        assertTrue(result.getOutput().contains("Hello from plugin 'com.playframework.twirl'"));
-    }
-
-    private void writeString(File file, String string) throws IOException {
-        try (Writer writer = new FileWriter(file)) {
-            writer.write(string);
-        }
-    }
+  }
 }
