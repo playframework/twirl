840ede8af62530591b5876b9da983b79461c609e
Sergey Morgunov <hostage@me.com>
2023-06-19 14:20:09 +0300

    Enable CI for Gradle Plugin

--- gradle-twirl/build.gradle.kts
+++ gradle-twirl/build.gradle.kts
@@ -2,11 +2,20 @@
 * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
 */
 
+import java.time.Duration
+import java.util.Base64
+import kotlin.text.Charsets.UTF_8
+
 plugins {
-    // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
-    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.2.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     id("com.diffplug.spotless") version "6.19.0"
+    signing
 }
+
+// group = "com.playframework" // group and plugin id must use same top level namespace
+group = "com.typesafe.play" // TODO: uncomment line above and remove this
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -38,10 +47,29 @@ testing {
     }
 }
 
+signing {
+    val signingKey = Base64.getDecoder().decode(System.getenv("PGP_SECRET").orEmpty()).toString(UTF_8)
+    val signingPassword = System.getenv("PGP_PASSPHRASE").orEmpty()
+    useInMemoryPgpKeys(signingKey, signingPassword)
+}
+
+nexusPublishing {
+    packageGroup.set(project.group.toString())
+    clientTimeout.set(Duration.ofMinutes(60))
+    this.repositories {
+        sonatype()
+    }
+}
+
 gradlePlugin {
+    website.set("https://www.playframework.com/documentation/latest/ScalaTemplates")
+    vcsUrl.set("https://github.com/playframework/twirl")
     // Define the plugin
     val greeting by plugins.creating {
-        id = "com.playframework.twirl"
+        id = "com.typesafe.play.twirl" // TODO: rename to "com.playframework.twirl"
+        displayName = "Twirl Plugin"
+        description = "A Gradle plugin to compile Twirl templates"
+        tags.set(listOf("playframework", "web", "template", "java", "scala"))
         implementationClass = "play.twirl.gradle.TwirlPlugin"
     }
 }
