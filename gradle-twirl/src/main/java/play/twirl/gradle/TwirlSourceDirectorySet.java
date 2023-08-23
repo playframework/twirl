274c25e2c99fa31cce02fcdbbf599c18899c64b0
Sergey Morgunov <hostage@me.com>
2023-08-23 16:59:12 +0300

    Create SourceDirectorySet for Twirl

---
+++ gradle-twirl/src/main/java/play/twirl/gradle/TwirlSourceDirectorySet.java
@@ -0,0 +1,12 @@
+/*
+* Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+*/
+package play.twirl.gradle;
+
+import org.gradle.api.file.SourceDirectorySet;
+
+/**
+ * A {@code TwirlSourceDirectorySet} defines the properties and methods added to a {@link
+ * org.gradle.api.tasks.SourceSet} by the {@code TwirlPlugin}.
+ */
+public interface TwirlSourceDirectorySet extends SourceDirectorySet {}
---
+++ gradle-twirl/src/main/java/play/twirl/gradle/internal/DefaultTwirlSourceDirectorySet.java
@@ -0,0 +1,19 @@
+/*
+* Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+*/
+package play.twirl.gradle.internal;
+
+import javax.inject.Inject;
+import org.gradle.api.file.SourceDirectorySet;
+import org.gradle.api.internal.file.DefaultSourceDirectorySet;
+import org.gradle.api.internal.tasks.TaskDependencyFactory;
+import play.twirl.gradle.TwirlSourceDirectorySet;
+
+public class DefaultTwirlSourceDirectorySet extends DefaultSourceDirectorySet
+    implements TwirlSourceDirectorySet {
+  @Inject
+  public DefaultTwirlSourceDirectorySet(
+      SourceDirectorySet sourceDirectorySet, TaskDependencyFactory taskDependencyFactory) {
+    super(sourceDirectorySet, taskDependencyFactory); // Gradle 8+
+  }
+}
