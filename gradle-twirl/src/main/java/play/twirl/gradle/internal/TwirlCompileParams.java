be6925df18a7ee01acbcb262f14afbc755c5b244
Sergey Morgunov <hostage@me.com>
2023-08-28 17:27:53 +0300

    [Gradle] Add destination directory to the action params

--- gradle-twirl/src/main/java/play/twirl/gradle/internal/TwirlCompileParams.java
+++ gradle-twirl/src/main/java/play/twirl/gradle/internal/TwirlCompileParams.java
@@ -1,12 +1,15 @@
 /*
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
-package play.twirl.gradle;
+package play.twirl.gradle.internal;
 
+import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.workers.WorkParameters;
 
 public interface TwirlCompileParams extends WorkParameters {
 
   RegularFileProperty getSourceFile();
+
+  DirectoryProperty getDestinationDirectory();
 }
