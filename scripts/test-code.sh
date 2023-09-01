03be4efddc258eb823650a8bdb3762cba5190070
Sergey Morgunov <hostage@me.com>
2023-09-01 19:11:26 +0300

    [Gradle] Add functional tests

--- scripts/test-code.sh
+++ scripts/test-code.sh
@@ -3,5 +3,5 @@
 # Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
 
 sbt "++$MATRIX_SCALA test" || exit 1
-sbt +publishLocal +compiler/publishM2 plugin/test plugin/scripted || exit 1
-(cd gradle-twirl && ./gradlew clean check -x spotlessCheck --no-daemon) || exit 1
+sbt +publishLocal +compiler/publishM2 +apiJVM/publishM2 plugin/test plugin/scripted || exit 1
+(cd gradle-twirl && ./gradlew clean check -x spotlessCheck --no-daemon -Pscala.version="$MATRIX_SCALA") || exit 1
