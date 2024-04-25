bd3d4d8a6499fe5aa33db7f4a706ccab3f8a37f0
Sergey Morgunov <hostage@me.com>
2024-04-25 15:19:42 +0300

    Bump Gradle to latest 8.7 for plugin tests

--- gradle-twirl/src/test/java/play/twirl/gradle/AbstractFunctionalTest.java
+++ gradle-twirl/src/test/java/play/twirl/gradle/AbstractFunctionalTest.java
@@ -100,12 +100,12 @@ abstract class AbstractFunctionalTest {
   static Stream<String> gradleVersions() {
     // https://docs.gradle.org/current/userguide/scala_plugin.html#sec:configure_zinc_compiler
     if (getScalaVersion().equals("3")) { // Gradle 7.5+
-      return Stream.of("7.6.2", "8.0.2", "8.4");
+      return Stream.of("7.6.2", "8.0.2", "8.7");
     }
     // https://docs.gradle.org/current/userguide/compatibility.html
     if (JavaVersion.current().compareTo(JavaVersion.VERSION_17) >= 0) { // Gradle 7.3+
-      return Stream.of("7.6.2", "8.0.2", "8.4");
+      return Stream.of("7.6.2", "8.0.2", "8.7");
     }
-    return Stream.of("7.1.1", "7.6.2", "8.0.2", "8.4");
+    return Stream.of("7.1.1", "7.6.2", "8.0.2", "8.7");
   }
 }
