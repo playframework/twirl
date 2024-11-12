30f7d61199cf62ccffa9753db27bc60a63003606
Scala Steward <m.kurz@irregular.at>
2024-11-12 22:32:27 +0100

    [2.0.x] scala3-compiler 3.3.4 (was 3.3.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@
 object Dependencies {
   val Scala212      = "2.12.20"
   val Scala213      = "2.13.15"
-  val Scala3        = "3.3.3"
+  val Scala3        = "3.3.4"
   val ScalaVersions = Seq(Scala212, Scala213, Scala3)
 }
