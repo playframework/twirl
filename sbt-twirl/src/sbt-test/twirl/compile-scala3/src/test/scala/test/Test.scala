31baecf5e6899a825d8cb2cbe240c032142908d7
xuwei-k <6b656e6a69@gmail.com>
2026-04-05 16:03:38 +0900

    Avoid deprecated scala.App

--- sbt-twirl/src/sbt-test/twirl/compile-scala3/src/test/scala/test/Test.scala
+++ sbt-twirl/src/sbt-test/twirl/compile-scala3/src/test/scala/test/Test.scala
@@ -2,44 +2,46 @@ package test
 
 import play.twirl.api.*
 
-object Test extends App {
+object Test {
   def test(template: HtmlFormat.Appendable, expected: String) = {
     assert(template.body == expected, s"Found '$template' but expected '$expected'")
   }
 
-  test(a.b.html.c.render("world"), "Hello, world.\n")
+  def main(args: Array[String]): Unit = {
+    test(a.b.html.c.render("world"), "Hello, world.\n")
 
-  test(html.template.render("42"), "Answer: 42\n")
+    test(html.template.render("42"), "Answer: 42\n")
 
-  test(
-    html.manyargstemplate.render(
-      "1",
-      "2",
-      "3",
-      "5",
-      "4",
-      "7",
-      "6",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-      "24",
-      "25",
-      "26"
-    ),
-    "26 args: 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26\n"
-  )
+    test(
+      html.manyargstemplate.render(
+        "1",
+        "2",
+        "3",
+        "5",
+        "4",
+        "7",
+        "6",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23",
+        "24",
+        "25",
+        "26"
+      ),
+      "26 args: 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26\n"
+    )
+  }
 }
